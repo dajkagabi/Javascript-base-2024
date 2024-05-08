e006c539501b678b2a0d3c657ee675e96c122e6f
Gabriella Dajka <dajkagabi01@gmail.com>
2024-05-08 11:41:47 +0200

    adatok feltöltése

---
+++ variable.js
@@ -0,0 +1,12 @@
+//let, var, const
+let firstName = "Gabriella";
+let lastName = "Dajka";
+let age = 23;
+
+console.log(firstName, lastName, age);
+console.log(`Név: ${firstName} ${lastName}, életkor: ${age}`); 
+
+let Name1 = "John";
+let Name2 = "Fred";
+let age1 = 21;
+console.log("Név: " + Name1 + " " + Name2 + ", életkor: " + age1);
