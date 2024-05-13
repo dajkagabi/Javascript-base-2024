e4afffdbfd4809c6178fbf1bdd48ff27c98a42cd
Gabriella Dajka <dajkagabi01@gmail.com>
2024-05-13 10:02:47 +0200

    folyamatban

--- variable.js
+++ variable.js
@@ -1,7 +1,16 @@
-//let, var, const
+//let, String, Number
 let firstName = "Gabriella";
 let lastName = "Dajka";
+
+//Number
 let age = 23;
+
+//var
+var welcome = "Hello Javascript";
+console.log(welcome);
+//const
+const PI = 3.14159; 
+console.log(PI);
 
 console.log(firstName, lastName, age);
 console.log(`Név: ${firstName} ${lastName}, életkor: ${age}`); 
@@ -10,3 +19,35 @@ let Name1 = "John";
 let Name2 = "Fred";
 let age1 = 21;
 console.log("Név: " + Name1 + " " + Name2 + ", életkor: " + age1);
+
+//Boolen
+/*const login= true;
+const logout = false;
+console.log(logout, logged_in); */
+
+
+//Null
+let number = null;
+console.log(number);
+
+//Undefined
+const score = undefined;
+console.log(score);
+
+
+//Array
+const car = ['OPEL', 'SKODA', 'HYUNDAI', 'VOLVO',]
+console.log(car);
+
+//PUSH
+car.push("NISSAN", "SEAT");
+console.log(car);
+
+//POP
+car.pop();
+console.log(car);
+
+//LENGHT
+car.length;
+console.log(car);
+
