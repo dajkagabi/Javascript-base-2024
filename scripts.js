a290d8f5004098d29eb1192de76a6ed5910005b0
Gabriella Dajka <dajkagabi01@gmail.com>
2024-05-13 09:52:40 +0200

    folyamatban

---
+++ scripts.js
@@ -0,0 +1,30 @@
+console.log("Hello");
+//alert("Hello");
+//console.clear();
+console.error("Error");
+console.warn("This is a warning message.");
+console.info('Console INFO')
+console.table({name: 'Gabriella', email: 'hello2024@gmail.com'});
+console.table(["OPEL", "VOLVO", "FORD"] );
+
+
+
+console.debug("Debugging information: ");
+
+
+//console.groupCollapsed("Error");
+console.group("Error 1");
+console.log("Error 2");
+console.log("Error 3");
+console.log("Error 4");
+console.groupEnd();
+
+//console.groupCollapsed("Network");
+console.group("Network REQ");
+console.log("GET");
+console.log("POST");
+console.log("DELETE");
+console.groupEnd();
+
+
+ 
