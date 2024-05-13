10740e566e13c060302cb814b9738c56a6f4f5ec
Gabriella Dajka <dajkagabi01@gmail.com>
2024-05-13 11:09:43 +0200

    folyamatban

---
+++ operators.js
@@ -0,0 +1,28 @@
+//Aretmetiaki
+let x = 5;
+let y = 5;
+
+
+let z = x + y; 
+let q = y - x; 
+let r = x * y; 
+let s = y / x;
+let t = y % x; 
+
+console.log(z, q, r, s, t);
+
+//Összehasonlító
+let a = 10;
+let b = 5;
+
+console.log(a > b); 
+console.log(a < b); 
+console.log(a === b); 
+console.log(a !== b); 
+
+//Logikai
+let sunny = true;
+let warm = false;
+console.log(sunny && warm); 
+console.log(sunny || warm); 
+console.log(!sunny);
