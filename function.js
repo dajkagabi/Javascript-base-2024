08a9f34d1d40ec5bd6dca9e49bd43af257d58e93
Gabriella Dajka <dajkagabi01@gmail.com>
2024-05-13 10:45:56 +0200

    folyamatban

---
+++ function.js
@@ -0,0 +1,14 @@
+function greet (name) {
+    console.log('Hello '  + name);
+}
+
+greet('Gabriella');
+
+
+
+function greet2 (string, lastSring){
+    console.log('' + string + ' ' + lastSring);
+}
+
+greet2('Good morning', 'Everybody');
+greet2('God morgen', 'Everybody');
